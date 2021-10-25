9d33b08fae70e8d957a796b298741addbb1e0d32
muhamuttaqien <muha.muttaqien@gmail.com>
2021-10-25 16:51:30 +0900

    Simpan semua file pert-1

---
+++ Pert-1-Casting.py
@@ -0,0 +1,21 @@
+x = 1.75
+
+print(int(x))
+
+y = 9
+
+print(float(y))
+
+y2 = str(y)
+
+print(type(y2))
+
+x = 2*6/3 # float
+x = int(x)
+
+print(type(x)) # integer
+
+check = 0.7 + 1
+print(type(check))
+
+print(round(0.6))
---
+++ Pert-1.py
@@ -0,0 +1,22 @@
+# print adalah keyword dari python untuk mencetak text
+print('Hello, Angga!')
+
+# jangan utak-atik bagian kodingan ini
+
+# membuat variables
+x1 = 5 # integer
+x2 = 2 # integer
+profil_angga = "Nama saya Angga, saya berdomisili di Tokyo, saya adalah profesional di bidang AI" # string
+
+print(x1 ** x2)
+print(profil_angga)
+
+var1 = 5 # integer (int)
+var2 = "susunan kalimat" # string (str)
+var3 = 0.75 # float
+var4 = False # boolean (bool)
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
